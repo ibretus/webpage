ac44b629a27f9952c6252b167e81e60acb84b402
ibretus <ibretus@gmail.com>
2016-10-12 20:12:13 +1000

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.getElementById("nav").innerHTML =
 								
 document.getElementById("head").innerHTML =
 								'<title>AROcustomleather</title>'+
-								'<link rel="stylesheet" type="text/css" href="../css/main.css" />'+
+								'<link rel="stylesheet" type="text/css" href="main.css" />'+
 								'<meta name="viewport " content="width=device-width, initial-scale=1.0">'+
 								'<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>'+
 								'<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>'+
