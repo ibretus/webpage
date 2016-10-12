142352b8c4bc93e6fb453ac659027015e480a315
ibretus <ibretus@gmail.com>
2016-10-12 20:04:11 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("nav").innerHTML =
+							'<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">'+
+								'<ul class="nav navbar-nav">'+                                
+									'<li><a class="selected" href="index.html">Home</a></li>'+
+									'<li><a class="selected" href="about.html">About</a></li>'+
+									'<li><a class="selected" href="contact.html">Contact</a></li>'+
+									'<li><a class="selected" href="https://www.facebook.com/customleatheraustralia/?ref=aymt_homepage_panel"target="_blank">Face book</a></li>'+
+									'<li><a class="selected" href=" https://paypal.me/aroleather"target="blank">PayPal</a></li>'+
+									'<li><a class="selected" href="gallery.html">Gallery</a></li>'+
+								'</ul>'+
+							'</nav>'+
+							'<link rel="stylesheet" type="text/css" href="../css/main.css" />';							
+								
+document.getElementById("head").innerHTML =
+								'<title>AROcustomleather</title>'+
+								'<link rel="stylesheet" type="text/css" href="../css/main.css" />'+
+								'<meta name="viewport " content="width=device-width, initial-scale=1.0">'+
+								'<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>'+
+								'<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>'+
+								'<script type="text/javascript" src="script.js" ></script>';
+								
+document.getElementById("footer").innerHTML =
+								 'copyright &copy;2016 Brett Dunk';
+								 
+document.getElementById("header").innerHTML =
+								'<h2>Australian Red Ochre</h2>'+
+								'<h1>CUSTOM LEATHER</h1>';
